08708d37acf1d6b351e160b387d2c1e029702a94
angeliquenayanka@icloud.com <ANGELIQUE.RUMSEY88@student.qcc.cuny.edu>
2024-02-12 19:16:49 -0800

    Added text property of Form and added a label

--- rumsey_class assigment/Form1.cs
+++ rumsey_class assigment/Form1.cs
@@ -6,5 +6,10 @@ namespace rumsey_class_assigment
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
